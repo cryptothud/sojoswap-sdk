7b1e0d6a21c356184445742222add70a7758b135
Richard Dwi <dwi@reimu>
2022-05-09 11:22:35 -0400

    run build

---
+++ src/constants.d.ts
@@ -0,0 +1,12 @@
+import JSBI from 'jsbi';
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export interface FACTORY_CONFIG {
+    FACTORY_ADDRESS: string;
+    INIT_CODE_HASH: string;
+}
+export declare const UNISWAP_DEFAULT_CONFIG: FACTORY_CONFIG;
---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+export { FACTORY_CONFIG, MINIMUM_LIQUIDITY } from './constants';
+export * from './errors';
+export * from './entities';
+export * from './router';
