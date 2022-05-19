996bdaa46775e0ff1aa43b9e29c3963a2041704c
Richard Dwi <dwi@reimu>
2022-05-18 21:39:31 -0400

    change the prepublishOnly script to a prepare script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export { FACTORY_CONFIG, MINIMUM_LIQUIDITY } from './constants'
+export { FACTORY_CONFIG, MINIMUM_LIQUIDITY, UNISWAP_DEFAULT_CONFIG } from './constants'
 
 export * from './errors'
 export * from './entities'
 export * from './router'
+export * from '@uniswap/sdk-core'
+
+export enum ChainId {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    GÖRLI = 5,
+    KOVAN = 42
+}
